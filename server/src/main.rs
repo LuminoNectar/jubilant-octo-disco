b1b22e6aa7fd067ed7fc998891c9038e8818fad9
Adam <ajeniski4@gmail.com>
2023-05-17 07:53:49 -0400

    add db pool

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,34 @@
-use axum::{
-    routing::get,
-    Router,
-};
+use anyhow::Result;
+use axum::{routing::get, Router};
+use sqlx::{Executor, SqlitePool};
 
 #[tokio::main]
-async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+async fn main() -> Result<()> {
+    // setup db pool
+    let db_file = "db.sqlite"; // replace with env var
+    let conn_str = format!("sqlite:{db_file}?mode=rwc"); // read, write, create
+    let pool = SqlitePool::connect(&conn_str).await?;
 
-    // run it with hyper on localhost:3000
+    // setup db tables
+    pool.execute(
+        r#"
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL
+        )
+        "#,
+    )
+    .await?;
+
+    // setup routes
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .with_state(pool);
+
+    // start server
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .await?;
+
+    Ok(())
 }
