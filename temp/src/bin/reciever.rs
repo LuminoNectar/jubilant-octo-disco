8da127d52e8b7f5e1b857c7a52b85bd4b253de7c
John <johnvictorwilliams@gmail.com>
2023-06-02 16:51:12 -0400

    Non working file streaming

--- temp/src/bin/reciever.rs
+++ temp/src/bin/reciever.rs
@@ -1,12 +1,16 @@
 use srt_tokio::SrtSocket;
 use std::io::Error;
 use tokio_stream::StreamExt;
+use tokio::io::AsyncWriteExt;
+use tokio::fs::File;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let mut srt_socket = SrtSocket::builder().call("127.0.0.1:3333", None).await?;
+    let file = File::create("temp.pdf").await?;
+    let mut write_buffer = tokio::io::BufWriter::new(file);
     while let Some((_instant, bytes)) = srt_socket.try_next().await? {
-        println!("{bytes:?}");
+       write_buffer.write(&bytes).await?; 
     }
 
     println!("\nConnection closed");
--- temp/src/bin/sender.rs
+++ temp/src/bin/sender.rs
@@ -1,22 +1,44 @@
-use bytes::Bytes;
+use bytes::{Bytes, BytesMut};
 use futures::stream;
 use futures::{SinkExt, StreamExt};
 use srt_tokio::SrtSocket;
 use std::io::Error;
 use std::time::{Duration, Instant};
+use tokio::io::AsyncReadExt;
 use tokio::time::sleep;
+use tokio::fs::File;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let mut srt_socket = SrtSocket::builder().listen_on(":3333").await?;
 
-    let mut stream = stream::unfold(0, |count| async move {
-        if count > 1000 {
-            return None;
+    //Open video file using std::fs::files to read into bytes and transmit
+    let file = File::open("Keybinds.pdf").await?;
+
+    let mut read_buffer = tokio::io::BufReader::new(file);
+    //let mut list_of_chunks = vec::new();
+    //let chunk_size = 0x4000;
+
+    // loop {
+    //     let mut chunk = vec::with_capacity(chunk_size);
+    //     let n = file.by_ref().take(chunk_size as u64).read_to_end(&mut chunk)?;
+    //     if n == 0 { break; }
+    //     list_of_chunks.push(chunk);
+    //     if n < chunk_size { break; }
+    // }
+    let mut stream = stream::unfold(&mut read_buffer, |read_buffer| async move {
+        let mut chunk = BytesMut::with_capacity(8192);
+        if let Ok(size) = read_buffer.read(chunk.as_mut()).await {
+            if size == 0 {
+                return None;
+            }
+
+            print!("\rSent {size:?} packets");
+            sleep(Duration::from_millis(10)).await;
+            Some((Ok((Instant::now(),Bytes::from(chunk))),read_buffer))
+        } else {
+            None
         }
-        print!("\rSent {count:?} packets");
-        sleep(Duration::from_millis(10)).await;
-        Some((Ok((Instant::now(), Bytes::from(std::format!("msg: {}", count)))), count + 1))
     })
     .boxed();
 
