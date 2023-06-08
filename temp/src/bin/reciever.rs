9be3e2914b85fe8fd55f520642ebd87e2c3166e3
John <johnvictorwilliams@gmail.com>
2023-06-07 21:59:16 -0400

    Move flush and send video

--- temp/src/bin/reciever.rs
+++ temp/src/bin/reciever.rs
@@ -8,13 +8,17 @@ use tokio_stream::StreamExt;
 async fn main() -> Result<(), Error> {
     let mut srt_socket =
         SrtSocket::builder().call("127.0.0.1:3333", None).await?;
-    let file = File::create("other_file.txt").await?;
+    let file = File::create("other_file.mp4").await?;
     let mut write_buffer = tokio::io::BufWriter::new(file);
+    let mut count = 0;
     while let Some((_instant, bytes)) = srt_socket.try_next().await? {
-        println!("Received {:?}", bytes);
+        //println!("Received {:?}", bytes);
+        // print!("\r{count}");
+        count += 1;
         write_buffer.write_all(&bytes).await?;
+        write_buffer.flush().await?;
+
     }
-    write_buffer.flush().await?;
 
     println!("\nConnection closed");
 
--- temp/src/bin/sender.rs
+++ temp/src/bin/sender.rs
@@ -13,18 +13,20 @@ async fn main() -> Result<(), Error> {
     let mut srt_socket = SrtSocket::builder().listen_on(":3333").await?;
 
     //Open video file using std::fs::files to read into bytes and transmit
-    let file = File::open("temp.txt").await?;
+    let file = File::open("video.mp4").await?;
     let read_buffer = tokio::io::BufReader::with_capacity(8192, file);
 
-    let mut stream = stream::unfold(read_buffer, |mut read_buffer| async move {
+    let mut stream = stream::unfold((read_buffer, 0), |(mut read_buffer,count)| async move {
+
         let chunk = read_buffer.fill_buf().await.ok()?.to_vec();
         if chunk.is_empty() {
             return None;
         }
+        // print!("\r{count}");
         read_buffer.consume(chunk.len());
 
         sleep(Duration::from_millis(10)).await;
-        Some((Ok((Instant::now(),Bytes::from(chunk))), read_buffer))
+        Some((Ok((Instant::now(),Bytes::from(chunk))), (read_buffer,count+1)))
     })
     .boxed();
 
